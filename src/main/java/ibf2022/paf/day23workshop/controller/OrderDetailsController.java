4b0c3942439725afe41c9367fb67a2dd6cd61bf7
Woody3797 <woody3797@gmail.com>
2023-04-26 14:46:52 +0800

    day23workshop

--- src/main/java/ibf2022/paf/day23workshop/controller/OrderDetailsController.java
+++ src/main/java/ibf2022/paf/day23workshop/controller/OrderDetailsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ibf2022.paf.day23workshop.model.OrderDetails;
 import ibf2022.paf.day23workshop.repository.OrderDetailsRepository;
@@ -29,6 +30,24 @@ public class OrderDetailsController {
         model.addAttribute("error", false);
         model.addAttribute("orderDetails", orderDetails);
         return "index";
+    }
+
+    @GetMapping(path = "/")
+    public String getOrderForm2(Model model, @ModelAttribute OrderDetails orderDetails, @RequestParam(required = false) Integer orderId) {
+        if (orderId == null) {
+            model.addAttribute("error", false);
+            model.addAttribute("display", false);
+            return "index2";
+        }
+        try {
+            orderDetails = repository.getOrderDetailsWithDiscount(orderId);
+            model.addAttribute("display", true);
+            model.addAttribute("orderDetails", orderDetails);
+            return "index2";
+        } catch (NullPointerException e) {
+            model.addAttribute("error", true);
+            return "index2";
+        }
     }
 
     @GetMapping(path = "{orderId}", produces = MediaType.TEXT_HTML_VALUE)
